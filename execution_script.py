a472ea932f49db67092a18f85e2ce9ce49e713c7
carlidel <carlidel@ymail.com>
2019-04-08 22:18:33 +0200

    More flexible resource manager

--- execution_script.py
+++ execution_script.py
@@ -1,6 +1,8 @@
 import multiprocessing
 import subprocess
 import shlex
+import os
+import sys
 
 from multiprocessing.pool import ThreadPool
 
@@ -10,17 +12,29 @@ def call_proc(cmd):
         cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     print(out, file=open('progress.txt', 'a'))
+    print(err, file=open('errors.txt', 'a'))
     return (out, err)
 
 
 if __name__ == "__main__":
-    pool = ThreadPool(multiprocessing.cpu_count())
+
+    if len(sys.argv) > 1:
+        CPUS = int(sys.argv[1])
+    else:
+        CPUS = multiprocessing.cpu_count()
+
+    if os.path.exists('progress.txt'):
+        os.remove('progress.txt')
+    if os.path.exists('errors.txt'):
+        os.remove('errors.txt')
+
+    pool = ThreadPool(CPUS)
     results = []
     with open("processed_pdb/names.txt", 'r') as f:
         names = [name[:-1] for name in f]
-    arg_names = [[] for i in range(multiprocessing.cpu_count())]
+    arg_names = [[] for i in range(CPUS)]
     for i, name in enumerate(names):
-        arg_names[i % multiprocessing.cpu_count()].append(name)
+        arg_names[i % CPUS].append(name)
     
     separator = " "
     for arg in arg_names:
